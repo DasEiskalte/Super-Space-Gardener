9d2f85771a975d31eb5154b76b7a4ded1bb513c9
DasEiskalte <49496115+DasEiskalte@users.noreply.github.com>
2020-02-06 20:56:30 +0100

    Hotfixes

--- Scripts/player.cs
+++ Scripts/player.cs
@@ -1,21 +1,29 @@
 using Godot;
 using System;
 
-public class player : KinematicBody2D
+public class Movement : KinematicBody2D
 {
-	// Declare member variables here. Examples:
-	// private int a = 2;
-	// private string b = "text";
-
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
+	public int speed = 200;
+	Vector2 velocity = new Vector2();
+	
+	public void GetInput()
 	{
+		velocity = new Vector2();
 		
+		if(Input.IsActionPressed("ui_right"))
+		{
+			velocity.x += 1;
+		}
+		if(Input.IsActionPressed("ui_left"))
+		{
+			velocity.x -= 1;
+		}
+		velocity = velocity.Normalized() * speed;
 	}
-
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
+	
+	public override void _PhysicsProcess(float delta)
+	{
+		GetInput();
+		velocity = MoveAndSlide(velocity);
+	}
 }
