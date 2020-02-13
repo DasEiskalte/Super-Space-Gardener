4ca0f097c40b4670b38f0bf092571c2f3281b5a9
DasEiskalte <49496115+DasEiskalte@users.noreply.github.com>
2020-02-13 20:15:28 +0100

    Improved Jump

--- Scripts/KinematicBody2D.gd
+++ Scripts/KinematicBody2D.gd
@@ -3,7 +3,7 @@ extends KinematicBody2D
 export (int) var speed = 200
 export (int) var speedSprint = 400
 export (int) var gravity = 40
-export (int) var jump = -6400
+export (int) var jump = 6400
 
 var velocity = Vector2()
 var up = Vector2(0, -1)
@@ -23,10 +23,8 @@ func get_input():
 func _physics_process(_delta):
 	get_input()
 	if is_on_floor() and Input.is_action_just_pressed("jump"):
-		velocity.y = jump
-	elif is_on_floor() and Input.is_action_pressed("jump") and (Input.is_action_just_pressed("ui_left") or Input.is_action_just_pressed("ui_right")):
-		
-		velocity.y = jump
-	if !is_on_floor():
+		velocity.y = -jump
+	
+	if !is_on_floor() or is_on_ceiling():
 		velocity.y += gravity
 	velocity = move_and_slide(velocity, up)
