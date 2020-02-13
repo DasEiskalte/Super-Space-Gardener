1e0854d2370add86abf3b4c0b916ef36b2400bbc
DasEiskalte <49496115+DasEiskalte@users.noreply.github.com>
2020-02-13 22:13:22 +0100

    Added crouching and uncrouching

--- Scripts/KinematicBody2D.gd
+++ Scripts/KinematicBody2D.gd
@@ -10,6 +10,11 @@ var up = Vector2(0, -1)
 
 func get_input():
 	velocity = Vector2()
+	if Input.is_action_just_pressed("crouch"):
+		crouch(1)
+		print("Crouch")
+	if Input.is_action_just_released("crouch"):
+		crouch(0)
 	if Input.is_action_pressed('ui_right'):
 		velocity.x += 1
 		get_node( "AnimatedSprite" ).set_flip_h( true )
@@ -24,9 +29,19 @@ func get_input():
 
 func _physics_process(_delta):
 	get_input()
+	
 	if is_on_floor() and Input.is_action_just_pressed("jump"):
 		velocity.y = -jump
-	
 	if !is_on_floor() or is_on_ceiling():
 		velocity.y += gravity
 	velocity = move_and_slide(velocity, up)
+
+func crouch(state):
+	if state:
+		$defaultHitbox.disabled = true
+		$crouchHitbox.disabled = false
+		print("Crouch")
+	else:
+		$defaultHitbox.disabled = false
+		$crouchHitbox.disabled = true
+		print("uncrouch")
