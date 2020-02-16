528605ede0175f86799fccec3f6b059ca0dff46c
Luca Schuster <lucaschuster@linux.fritz.box>
2020-02-16 10:25:37 +0100

    Bugfix and new jump animation

--- Scripts/KinematicBody2D.gd
+++ Scripts/KinematicBody2D.gd
@@ -2,13 +2,16 @@ extends KinematicBody2D
 
 export (int) var speed = 200
 export (int) var speedSprint = 400
-export (int) var gravity = 40
-export (int) var jump = 6400
+export (int) var gravity = 400
+export (int) var jump = 1000
 
-var velocity = Vector2()
+export (int) var counter = 0
+
+var velocity = Vector2() 
 var up = Vector2(0, -1)
 
 func get_input():
+	
 	velocity = Vector2()
 	if Input.is_action_just_pressed("crouch"):
 		crouch(1)
@@ -30,12 +33,29 @@ func get_input():
 func _physics_process(_delta):
 	get_input()
 	
-	if is_on_floor() and Input.is_action_just_pressed("jump"):
-		velocity.y = -jump
-	if !is_on_floor() or is_on_ceiling():
+	if  is_on_floor() and Input.is_action_just_pressed("jump"):
+		counter = 100
+		
+		
+	
+	
+	if !is_on_floor():
 		velocity.y += gravity
+		
+	if counter > 20:
+		velocity.y -= jump
+		counter -= 1
+		jump -= 15
+		
+	if counter == 20:
+		counter = 0
+		jump = 1000
+		
+	if is_on_ceiling():
+		counter = 20
+		
 	velocity = move_and_slide(velocity, up)
-
+	
 func crouch(state):
 	if state:
 		$defaultHitbox.disabled = true
@@ -47,3 +67,5 @@ func crouch(state):
 		$crouchHitbox.disabled = true
 		$AnimatedSprite.animation = "default"
 		print("uncrouch")
+
+
