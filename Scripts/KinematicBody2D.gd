c0019e5736659da4be73a643cf68c6afe79378c3
Luca Schuster <lucaschuster@linux.fritz.box>
2020-02-18 20:30:20 +0100

    added walk animation

--- Scripts/KinematicBody2D.gd
+++ Scripts/KinematicBody2D.gd
@@ -4,26 +4,22 @@ export (int) var speed = 200
 export (int) var speedSprint = 400
 export (int) var gravity = 600
 export (int) var jump = 1100
-
 export (int) var counter = 0
 
 var velocity = Vector2()
 var up = Vector2(0, -1)
 
+
 func get_input():
 
 	velocity = Vector2()
 
-
 	if Input.is_action_pressed('ui_right'):
 		velocity.x += 1
-
 
 		get_node( "AnimatedSprite" ).set_flip_h( true )
 	if Input.is_action_pressed('ui_left'):
 		velocity.x -= 1
-
-
 
 		get_node( "AnimatedSprite" ).set_flip_h( false )
 	if Input.is_action_pressed('sprint'):
@@ -31,34 +27,23 @@ func get_input():
 	else:
 		velocity = velocity.normalized() * speed
 
-		if !Input.is_action_pressed("ui_left") and !Input.is_action_pressed(" ui_right"):
-			$AnimatedSprite.animation = "default"
-			
 	if  is_on_floor() and Input.is_action_just_pressed("jump"):
 		counter = 100
 
-
-	if Input.is_action_pressed("left") or Input.is_action_pressed("right"):
+	if Input.is_action_pressed("ui_left") or Input.is_action_pressed("ui_right"):
 		$AnimatedSprite.animation = "walk"
 	elif Input.is_action_pressed("crouch"):
 		$defaultHitbox.disabled = true
 		$crouchHitbox.disabled = false
 		$AnimatedSprite.animation = "crouch"
-
-
 	else:
 		$defaultHitbox.disabled = false
 		$crouchHitbox.disabled = true
 		$AnimatedSprite.animation = "default"
 
 
-
-
-
 func _physics_process(_delta):
 	get_input()
-
-	
 
 	if !is_on_floor():
 		velocity.y += gravity
@@ -74,7 +59,5 @@ func _physics_process(_delta):
 
 	if is_on_ceiling():
 		counter = 20
-
-
 
 	velocity = move_and_slide(velocity, up)
