e4ffa0606bfe7c6a0553de063e4d44746813a857
DasEiskalte <49496115+DasEiskalte@users.noreply.github.com>
2020-03-02 15:15:04 +0100

    Reworked speed Multipiler

--- Scripts/KinematicBody2D.gd
+++ Scripts/KinematicBody2D.gd
@@ -1,10 +1,15 @@
 extends Actor
 
 
-export (int) var speedMultiplier = 2
+export (float) var speedMultiplier = 1.25
+export (float) var crouchMultiplier = 0.75
+var Multiplier = 1
 export (int) var jump = 1100
 var isCrouched = false
 
+onready var defaultHitbox = $defaultHitbox
+onready var crouchHitbox = $crouchHitbox
+onready var walkHitbox = $walkHitbox
 
 func _physics_process(delta: float) -> void:
 	var is_jump_interrupted: = Input.is_action_just_released("jump") and _velocity.y < 0.0
@@ -12,9 +17,12 @@ func _physics_process(delta: float) -> void:
 	_velocity = calculate_move_velocity(_velocity, direction, speed, is_jump_interrupted)
 	
 	if Input.is_action_pressed("sprint"):
-		_velocity = Vector2(_velocity.x * speedMultiplier, _velocity.y)
+		Multiplier = speedMultiplier
+	if Input.is_action_just_released("sprint"):
+		Multiplier = 1
 	if Input.is_action_just_released("crouch"):
 		default()
+		Multiplier = 1
 	if Input.is_action_pressed("ui_left"):
 		if !isCrouched:
 			walk()
@@ -27,7 +35,7 @@ func _physics_process(delta: float) -> void:
 		default()
 	if Input.is_action_pressed("crouch"):
 		crouch()
-		
+		Multiplier = crouchMultiplier
 	
 	var snap: Vector2 = Vector2.DOWN * 60.0 if direction.y == 0.0 else Vector2.ZERO
 	_velocity = move_and_slide_with_snap(
@@ -48,6 +56,7 @@ func calculate_move_velocity(
 		speed: Vector2,
 		is_jump_interrupted: bool
 	) -> Vector2:
+	speed.x *= Multiplier
 	var velocity: = linear_velocity
 	velocity.x = speed.x * direction.x
 	if direction.y != 0.0:
