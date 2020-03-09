33b1a49685081d22851c3e38ff39a813c9666be1
DasEiskalte <49496115+DasEiskalte@users.noreply.github.com>
2020-03-09 17:24:36 +0100

    Added Raycasts

--- Scripts/KinematicBody2D.gd
+++ Scripts/KinematicBody2D.gd
@@ -21,7 +21,7 @@ func _physics_process(delta: float) -> void:
 	var is_jump_interrupted: = Input.is_action_just_released("jump") and _velocity.y < 0.0
 	var direction: = get_direction()
 	_velocity = calculate_move_velocity(_velocity, direction, speed, is_jump_interrupted)
-	if $RayCast2D.is_colliding() and (state == "crouch" or state == "crouchWalk"):
+	if $raycastCrouch.is_colliding() and (state == "crouch" or state == "crouchWalk"):
 			canUncrouch = false
 	else:
 			canUncrouch = true
@@ -83,10 +83,8 @@ func _physics_process(delta: float) -> void:
 		$crouchHitbox.disabled = true
 		$AnimatedSprite.animation = "walk"
 		Multiplier = speedMultiplier
-	print(canUncrouch)
 		
-		
-	
+	print(_check_is_valid_wall())
 	var snap: Vector2 = Vector2.DOWN * 60.0 if direction.y == 0.0 else Vector2.ZERO
 	_velocity = move_and_slide_with_snap(
 		_velocity, snap, FLOOR_NORMAL, true
@@ -118,3 +116,10 @@ func calculate_move_velocity(
 func reset():
 	get_tree().reload_current_scene()
 	
+func _check_is_valid_wall():
+		if $wallslideLeft.is_colliding():
+			return true
+		elif $wallslideRight.is_colliding():
+			return true
+		else:
+			return false
